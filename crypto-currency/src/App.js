356d0892f2a083a2842032cd615ec5d320e21cd2
Jinnie-kim <hjk7639@gmail.com>
2022-08-17 16:59:22 +0900

    style: basic UI style

--- crypto-currency/src/App.js
+++ crypto-currency/src/App.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 function App() {
+  const [loading, setLoading] = useState(true);
   return (
     <div className="App">
-      <h1>test2</h1>
+      <h1>🤡 Cryptocurrency Calculator 🤡</h1>
+      {loading ? <strong>Loading...👻</strong> : null}
+      <select>
+        <option></option>
+      </select>
+      <hr />
+      <input type='number'/>
+      <strong>USD</strong>
+      <br />
+      <input type='number'/>
+      <strong>crypto단위</strong>
     </div>
   );
 }
