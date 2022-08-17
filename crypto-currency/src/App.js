c1249760a644eb3c748e07537feb873edf783435
Jinnie-kim <hjk7639@gmail.com>
2022-08-17 17:33:06 +0900

    feat: crypto fetch request & make options with json data

--- crypto-currency/src/App.js
+++ crypto-currency/src/App.js
@@ -1,20 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => setCoins(json));
+    setLoading(false);
+  });
   return (
     <div className="App">
       <h1>🤡 Cryptocurrency Calculator 🤡</h1>
-      {loading ? <strong>Loading...👻</strong> : null}
-      <select>
-        <option></option>
-      </select>
-      <hr />
-      <input type='number'/>
-      <strong>USD</strong>
-      <br />
-      <input type='number'/>
-      <strong>crypto단위</strong>
+      {loading ? (
+        <strong>Loading...👻</strong>
+      ) : (
+        <div>
+          <select>
+            {coins.map((coin) => (
+              <option key={coin.id}>{coin.name}</option>
+            ))}
+          </select>
+          <hr />
+          <input type="number" />
+          <label>USD</label>
+          <br />
+          <input type="number" />
+          <label>crypto단위</label>
+        </div>
+      )}
     </div>
   );
 }
