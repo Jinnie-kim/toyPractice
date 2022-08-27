10500e70dfcfcc0363621a0b7c429d1e7dbdcbbe
Jinnie-kim <hjk7639@gmail.com>
2022-08-27 10:38:30 +0900

    필터링 로직 구현 & 필요없는 코드 삭제

--- shoppingmall/main.js
+++ shoppingmall/main.js
@@ -1,12 +1,6 @@
 const shopItemList = document.querySelector('.itemLists');
 const logoButton = document.querySelector('.logoButton');
 const buttons = document.querySelector('.shoppingmallButton');
-const blueButton = document.querySelector('.blue');
-const yellowButton = document.querySelector('.yellow');
-const pinkButton = document.querySelector('.pink');
-const tShirtButton = document.querySelector('.tshirtButton');
-const pantsButton = document.querySelector('.pantsButton');
-const skirtButton = document.querySelector('.skirtButton');
 
 loadShopData().then((items) => {
   showItem(items);
@@ -20,6 +14,7 @@ async function loadShopData() {
 }
 
 function showItem(items) {
+  shopItemList.innerHTML = '';
   items.map((item) => shopItemList.append(makeShopListItem(item)));
 }
 
@@ -36,94 +31,19 @@ function makeShopListItem(item) {
 
 function setButtonFilterEvent(items) {
   logoButton.addEventListener('click', () => showItem(items));
+  buttons.addEventListener('click', (event) =>
+    onFilterButtonClick(event, items)
+  );
 }
 
-async function loadBlueItemData() {
-  const response = await fetch('./data/filter.json');
-  const json = await response.json();
-  return json.blue;
+function onFilterButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  showItem(items.filter((item) => item[key] === value));
 }
-
-function filterBlueItem() {
-  shopItemList.innerHTML = '';
-  loadBlueItemData().then((items) => {
-    items.map((item) => shopItemList.append(makeShopListItem(item)));
-  });
-}
-
-blueButton.addEventListener('click', filterBlueItem);
-
-async function loadYellowItemData() {
-  const response = await fetch('./data/filter.json');
-  const json = await response.json();
-  return json.yellow;
-}
-
-function filterYellowItem() {
-  shopItemList.innerHTML = '';
-  loadYellowItemData().then((items) => {
-    items.map((item) => shopItemList.append(makeShopListItem(item)));
-  });
-}
-
-yellowButton.addEventListener('click', filterYellowItem);
-
-async function loadPinkItemData() {
-  const response = await fetch('./data/filter.json');
-  const json = await response.json();
-  return json.pink;
-}
-
-function filterPinkItem() {
-  shopItemList.innerHTML = '';
-  loadPinkItemData().then((items) => {
-    items.map((item) => shopItemList.append(makeShopListItem(item)));
-  });
-}
-
-pinkButton.addEventListener('click', filterPinkItem);
-
-async function loadTShirtItemData() {
-  const response = await fetch('./data/filter.json');
-  const json = await response.json();
-  return json.tShirt;
-}
-
-function filterTShirtItem() {
-  shopItemList.innerHTML = '';
-  loadTShirtItemData().then((items) => {
-    items.map((item) => shopItemList.append(makeShopListItem(item)));
-  });
-}
-
-tShirtButton.addEventListener('click', filterTShirtItem);
-
-async function loadPantsItemData() {
-  const response = await fetch('./data/filter.json');
-  const json = await response.json();
-  return json.pants;
-}
-
-function filterPantsItem() {
-  shopItemList.innerHTML = '';
-  loadPantsItemData().then((items) => {
-    items.map((item) => shopItemList.append(makeShopListItem(item)));
-  });
-}
-
-pantsButton.addEventListener('click', filterPantsItem);
-
-async function loadSkirtItemData() {
-  const response = await fetch('./data/filter.json');
-  const json = await response.json();
-  return json.skirt;
-}
-
-function filterSkirtItem() {
-  shopItemList.innerHTML = '';
-  loadSkirtItemData().then((items) => {
-    items.map((item) => shopItemList.append(makeShopListItem(item)));
-  });
-}
-
-skirtButton.addEventListener('click', filterSkirtItem);
