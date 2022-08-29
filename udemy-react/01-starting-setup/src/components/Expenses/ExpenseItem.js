a4bbff95576aba4207f97c22724be15d63767bd0
Jinnie-kim <hjk7639@gmail.com>
2022-08-29 21:52:15 +0900

    데모 코드 삭제

--- udemy-react/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ udemy-react/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -4,18 +4,13 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem({ title, date, amount }) {
-  const [newTitle, setNewTitle] = useState(title);
-  const clickHandler = () => {
-    setNewTitle('Updated!');
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2 className="expense-item">{newTitle}</h2>
+        <h2 className="expense-item">{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
