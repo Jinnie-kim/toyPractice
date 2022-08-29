7c78a358ad5b414dbe1ab0a4d03a64b6a1e17b6c
Jinnie-kim <hjk7639@gmail.com>
2022-08-29 09:14:55 +0900

    props를 통해 데이터 전달하기

--- udemy-react/01-starting-setup/src/components/ExpenseItem.js
+++ udemy-react/01-starting-setup/src/components/ExpenseItem.js
@@ -1,15 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+function ExpenseItem({ title, date, amount }) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2 className="expense-item">{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2 className="expense-item">{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
