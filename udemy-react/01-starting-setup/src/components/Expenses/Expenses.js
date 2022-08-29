bf488f7efbd5c5fe80297805e31911b28de048d4
Jinnie-kim <hjk7639@gmail.com>
2022-08-29 21:44:15 +0900

    year filtering 하기 & filtered year 전달하기

--- udemy-react/01-starting-setup/src/components/Expenses/Expenses.js
+++ udemy-react/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
-import './Expenses.css'
+import ExpenseFilter from './ExpenseFilter';
+import './Expenses.css';
 
 function Expenses({ expenses }) {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
-    <Card className='expenses'>
+    <Card className="expenses">
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
