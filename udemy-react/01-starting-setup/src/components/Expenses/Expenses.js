63872eff8a72bc8df1862afdb787357571ea3481
Jinnie-kim <hjk7639@gmail.com>
2022-08-30 09:22:38 +0900

    조건 반환 content 컴포넌트 분리하기

--- udemy-react/01-starting-setup/src/components/Expenses/Expenses.js
+++ udemy-react/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
 import ExpenseFilter from './ExpenseFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 function Expenses({ expenses }) {
@@ -16,34 +16,13 @@ function Expenses({ expenses }) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
-      {filteredExpenses.length > 0 &&
-        filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))} */}
-      {expensesContent}
+      <ExpensesList expenses={filteredExpenses} />
     </Card>
   );
 }
