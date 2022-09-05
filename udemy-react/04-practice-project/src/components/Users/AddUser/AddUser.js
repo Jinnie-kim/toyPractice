d5d27184fd809dc8ab91916af01cf72cf1fb04f9
Jinnie-kim <hjk7639@gmail.com>
2022-09-05 22:38:42 +0900

    유효성 검사 로직 추가하기

--- udemy-react/04-practice-project/src/components/Users/AddUser/AddUser.js
+++ udemy-react/04-practice-project/src/components/Users/AddUser/AddUser.js
@@ -19,6 +19,12 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
     setEnteredAge('');
     setEnteredUsername('');
     console.log(enteredUsername, enteredAge);
