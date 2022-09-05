150eb9a8cce672cd325f6935e02e4962899a876e
Jinnie-kim <hjk7639@gmail.com>
2022-09-05 21:34:15 +0900

    AddUser 컴포넌트 추가하기

--- udemy-react/04-practice-project/src/App.js
+++ udemy-react/04-practice-project/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import AddUser from './components/Users/AddUser/AddUser';
+import UserList from './components/Users/UserList/UserList';
 
 function App() {
   return (
     <div>
-
+      <section>
+        <AddUser />
+      </section>
     </div>
   );
 }
