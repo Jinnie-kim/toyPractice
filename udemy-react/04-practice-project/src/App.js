524e9495675379c69c0e2da00fd0cd05f3761f74
Jinnie-kim <hjk7639@gmail.com>
2022-09-07 11:53:34 +0900

    등록한 유저리스트 state lifting up 로직 & UserList 컴포넌트로 전달하는 로직 작성

--- udemy-react/04-practice-project/src/App.js
+++ udemy-react/04-practice-project/src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import AddUser from './components/Users/AddUser/AddUser';
 import UserList from './components/Users/UserList/UserList';
 
 function App() {
+  const [userInfo, setUserInfo] = useState([]);
+
+  const getUserInfoHandler = (enteredUsername, eneterdUserage) => {
+    setUserInfo((prevUser) => {
+      return [
+        ...prevUser,
+        {
+          name: enteredUsername,
+          age: eneterdUserage,
+          id: Math.random().toString(),
+        },
+      ];
+    });
+    // 내가 작성한 방식
+    // setUserInfo((prevUser) => {
+    //   let updateUser = [...prevUser];
+    //   updateUser.unshift({
+    //     name: enteredUsername,
+    //     age: eneterdUserage,
+    //     id: Math.random().toString(),
+    //   });
+    //   return updateUser;
+    // });
+  };
   return (
     <div>
-      <section>
-        <AddUser />
-      </section>
+      <AddUser onAddUser={getUserInfoHandler} />
+      <UserList users={userInfo} />
     </div>
   );
 }
