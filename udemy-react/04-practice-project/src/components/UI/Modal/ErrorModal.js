d930bf5c1bd2692c9df52ae9c8e89985119cf9eb
Jinnie-kim <hjk7639@gmail.com>
2022-09-07 15:41:34 +0900

    ErrorModal 클리어 함수 props로 받아서 추가

--- udemy-react/04-practice-project/src/components/UI/Modal/ErrorModal.js
+++ udemy-react/04-practice-project/src/components/UI/Modal/ErrorModal.js
@@ -8,7 +8,7 @@ import styles from './ErrorModal.module.css';
 const ErrorModal = (props) => {
   return (
     <div>
-      <div className={styles.backdrop}/>
+      <div className={styles.backdrop} onClick={props.onClearError} />
       <Card className={styles.modal}>
         <header className={styles.header}>
           <h2>{props.title}</h2>
@@ -17,7 +17,9 @@ const ErrorModal = (props) => {
           <p>{props.message}</p>
         </div>
         <footer className={styles.actions}>
-          <Button type="button">Okay</Button>
+          <Button type="button" onClick={props.onClearError}>
+            Okay
+          </Button>
         </footer>
       </Card>
     </div>
