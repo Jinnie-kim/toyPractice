a27e38ef6e2988ba2aa5d35b63e4980918658f15
Jinnie-kim <hjk7639@gmail.com>
2022-09-22 14:45:12 +0900

    use MealItem component to replace hard coded list

--- udemy-react/05-foodOrderApp/src/components/Meals/AvailableMeals.jsx
+++ udemy-react/05-foodOrderApp/src/components/Meals/AvailableMeals.jsx
@@ -1,4 +1,5 @@
 import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
@@ -35,7 +36,14 @@ const AvailableMeals = () => {
       <Card>
         <ul>
           {DUMMY_MEALS.map((meal) => {
-            return <li>{meal.name}</li>;
+            return (
+              <MealItem
+                key={meal.id}
+                name={meal.name}
+                description={meal.description}
+                price={meal.price}
+              />
+            );
           })}
         </ul>
       </Card>
