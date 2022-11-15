7f2a102362f3f748e8a306278a0c9879084b0775
Jinnie-kim <hjk7639@gmail.com>
2022-11-15 20:36:10 +0900

    user input이 비어있을 때 form 컨트롤하기

--- udemy-react/09-handlingForms/src/components/SimpleInput.js
+++ udemy-react/09-handlingForms/src/components/SimpleInput.js
@@ -10,12 +10,15 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+    if (enteredName.trim() == '') {
+      return;
+    }
     console.log(enteredName);
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
 
     setEnteredName('');
-    nameInputRef.current.value =''
+    nameInputRef.current.value = '';
   };
 
   return (
