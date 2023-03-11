8c711ae8b6a6c5cd7ff0cdb98d96d1147f637731
Jinnie <hjk7639@gmail.com>
2023-03-11 21:08:09 +0900

    이미지 경로 변경

--- rockSicssorPaper/src/main.ts
+++ rockSicssorPaper/src/main.ts
@@ -3,7 +3,8 @@ const score = document.querySelector('#score') as HTMLDivElement;
 const scissor = document.querySelector('#scissor') as HTMLButtonElement;
 const rock = document.querySelector('#rock') as HTMLButtonElement;
 const paper = document.querySelector('#paper') as HTMLButtonElement;
-const IMG_URL = './src/rsp.png';
+const IMG_URL =
+  'https://github.com/Jinnie-kim/toyPractice/blob/main/rockSicssorPaper/src/rsp.png?raw=true';
 
 const rspX = {
   scissor: '0',
@@ -13,3 +14,8 @@ const rspX = {
 
 computer.style.background = `url(${IMG_URL}) 0 0`; // background position으로 이미지의 가로위치 조정 바위: -220px, 보: -440px
 computer.style.backgroundSize = 'auto 200px'; // 높이 200px에 맞춰서 알아서 비율 조정(auto)
+
+setInterval(() => {
+  computer.style.background = `url(${IMG_URL}) 0 0`;
+  computer.style.backgroundSize = 'auto 200px';
+}, 50);
