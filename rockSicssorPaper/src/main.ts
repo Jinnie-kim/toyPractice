56b27854f72efdf6c734d0d61cc08edc217ac709
Jinnie <hjk7639@gmail.com>
2023-03-11 21:44:34 +0900

    가위바위보 0.1초 간격으로 이미지 변경하는 setInterval 구현

--- rockSicssorPaper/src/main.ts
+++ rockSicssorPaper/src/main.ts
@@ -5,7 +5,13 @@ const rock = document.querySelector('#rock') as HTMLButtonElement;
 const paper = document.querySelector('#paper') as HTMLButtonElement;
 const IMG_URL = './src/rsp.png';
 
-const rspX = {
+interface rspXCoord {
+  scissor: string;
+  rock: string;
+  paper: string;
+}
+
+const rspX: rspXCoord = {
   scissor: '0',
   rock: '-220px',
   paper: '-440px',
@@ -14,7 +20,21 @@ const rspX = {
 computer.style.background = `url(${IMG_URL}) 0 0`; // background position으로 이미지의 가로위치 조정 바위: -220px, 보: -440px
 computer.style.backgroundSize = 'auto 200px'; // 높이 200px에 맞춰서 알아서 비율 조정(auto)
 
+let coord = '0';
 setInterval(() => {
-  computer.style.background = `url(${IMG_URL}) 0 0`;
-  computer.style.backgroundSize = 'auto 200px';
-}, 50);
+  if (coord === rspX.scissor) {
+    // 가위인 경우
+    coord = rspX.rock;
+    computer.style.background = `url(${IMG_URL}) ${rspX.rock} 0`;
+    computer.style.backgroundSize = 'auto 200px';
+  } else if (coord === rspX.rock) {
+    // 바위인 경우
+    coord = rspX.paper;
+    computer.style.background = `url(${IMG_URL}) ${rspX.paper} 0`;
+    computer.style.backgroundSize = 'auto 200px';
+  } else if (coord === rspX.paper) {
+    coord = rspX.scissor;
+    computer.style.background = `url(${IMG_URL}) ${rspX.scissor} 0`;
+    computer.style.backgroundSize = 'auto 200px';
+  }
+}, 100);
