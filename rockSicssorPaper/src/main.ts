354be1782507ee45bb232bad70c8b5dda1cb5657
Jinnie <hjk7639@gmail.com>
2023-03-11 20:52:42 +0900

    가위, 바위, 보 이미지 스프라이트 x좌표 객체 생성, 가위 변수명 영어 스펠링 수정

--- rockSicssorPaper/src/main.ts
+++ rockSicssorPaper/src/main.ts
@@ -1,9 +1,15 @@
 const computer = document.querySelector('#computer') as HTMLDivElement;
 const score = document.querySelector('#score') as HTMLDivElement;
-const scissor = document.querySelector('#sicssor') as HTMLButtonElement;
+const scissor = document.querySelector('#scissor') as HTMLButtonElement;
 const rock = document.querySelector('#rock') as HTMLButtonElement;
 const paper = document.querySelector('#paper') as HTMLButtonElement;
 const IMG_URL = './src/rsp.png';
 
-computer.style.background = `url(${IMG_URL}) 0 0`;
-computer.style.backgroundSize = 'auto 200px';
+const rspX = {
+  scissor: '0',
+  rock: '-220px',
+  paper: '-440px',
+};
+
+computer.style.background = `url(${IMG_URL}) 0 0`; // background position으로 이미지의 가로위치 조정 바위: -220px, 보: -440px
+computer.style.backgroundSize = 'auto 200px'; // 높이 200px에 맞춰서 알아서 비율 조정(auto)
