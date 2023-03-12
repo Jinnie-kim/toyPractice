0097d79ed45d85b28d97e381eb2fee549eabd7b2
Jinnie <hjk7639@gmail.com>
2023-03-12 10:47:41 +0900

    가위바위보 포지션 바꾸는 로직 함수로 분리 & 컴퓨터의 이전 포지션 변수를 좀 더 명시적인 이름으로 변경 + 로직 변경

--- rockSicssorPaper/src/main.ts
+++ rockSicssorPaper/src/main.ts
@@ -17,24 +17,27 @@ const rspX: rspXCoord = {
   paper: '-440px',
 };
 
-computer.style.background = `url(${IMG_URL}) 0 0`; // background position으로 이미지의 가로위치 조정 바위: -220px, 보: -440px
-computer.style.backgroundSize = 'auto 200px'; // 높이 200px에 맞춰서 알아서 비율 조정(auto)
+// computer.style.background = `url(${IMG_URL}) 0 0`; // background position으로 이미지의 가로위치 조정 바위: -220px, 보: -440px
+// computer.style.backgroundSize = 'auto 200px'; // 높이 200px에 맞춰서 알아서 비율 조정(auto)
 
-let coord = '0';
-setInterval(() => {
-  if (coord === rspX.scissor) {
+let computerChoice = 'scissor';
+
+function changeComputerHand() {
+  if (computerChoice === 'scissor') {
     // 가위인 경우
-    coord = rspX.rock;
-    computer.style.background = `url(${IMG_URL}) ${rspX.rock} 0`;
-    computer.style.backgroundSize = 'auto 200px';
-  } else if (coord === rspX.rock) {
+    computerChoice = 'rock';
+  } else if (computerChoice === 'rock') {
     // 바위인 경우
-    coord = rspX.paper;
-    computer.style.background = `url(${IMG_URL}) ${rspX.paper} 0`;
-    computer.style.backgroundSize = 'auto 200px';
-  } else if (coord === rspX.paper) {
-    coord = rspX.scissor;
-    computer.style.background = `url(${IMG_URL}) ${rspX.scissor} 0`;
-    computer.style.backgroundSize = 'auto 200px';
+    computerChoice = 'paper';
+  } else if (computerChoice === 'paper') {
+    computerChoice = 'scissor';
   }
-}, 100);
+
+  // rspX.computerChoice -> 문자열 'computerChoice'를 찾고있기 때문에 틀린 코드
+  computer.style.background = `url(${IMG_URL}) ${
+    rspX[computerChoice as keyof rspXCoord]
+  } 0`;
+  computer.style.backgroundSize = 'auto 200px';
+}
+
+setInterval(changeComputerHand, 50); // setTimeout을 이용하여 재귀함수로 구현할 수도 있다.
