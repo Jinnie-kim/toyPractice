916c73fdeb747bbf2cb4db4c5109487880edb464
Jinnie <hjk7639@gmail.com>
2023-03-13 10:55:15 +0900

    Screen을 클릭했을 때, Screen 상태에 따라 class 이름을 변경해주어 Screen color를 바꿔주는 로직 작성

--- reactionVelocityCheck/src/main.ts
+++ reactionVelocityCheck/src/main.ts
@@ -2,3 +2,38 @@ import './style.css';
 
 const screen = document.querySelector('#screen') as HTMLDivElement;
 const result = document.querySelector('#result') as HTMLDivElement;
+
+interface ScreenStatus {
+  waiting: string;
+  ready: string;
+  now: string;
+}
+
+// 굳이 html에도 있는 데이터를 자바스크립트에서 한 번더 관리해 줄 필요는 없다.
+let screenStatus: ScreenStatus = {
+  waiting: 'waiting',
+  ready: 'ready',
+  now: 'now',
+};
+
+let screenMessage: string;
+
+screen.addEventListener('click', (event: Event) => {
+  if (
+    (event.currentTarget as HTMLDivElement).className === screenStatus.waiting
+  ) {
+    screenMessage = '초록색이 되면 클릭하세요';
+    (event.currentTarget as HTMLDivElement).className = screenStatus.ready;
+  } else if (
+    (event.currentTarget as HTMLDivElement).className === screenStatus.ready
+  ) {
+    screenMessage = '클릭하세요!';
+    (event.currentTarget as HTMLDivElement).className = screenStatus.now;
+  } else if (
+    (event.currentTarget as HTMLDivElement).classList.contains(screenStatus.now)
+  ) {
+    screenMessage = '클릭해서 시작하세요';
+    (event.currentTarget as HTMLDivElement).className = screenStatus.waiting;
+  }
+  screen.textContent = screenMessage;
+});
