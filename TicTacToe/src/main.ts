581a106268c93f7bb9a6518916631ee3c8170cbe
Jinnie <hjk7639@gmail.com>
2023-03-14 14:16:12 +0900

    승패 여부 계산하기, 무승부 계산하, 승리 후 클릭이벤트 제거

--- TicTacToe/src/main.ts
+++ TicTacToe/src/main.ts
@@ -8,6 +8,65 @@ const $result = document.createElement('div') as HTMLDivElement;
 let turn = '😼';
 const rows: HTMLTableCellElement[][] = [];
 
+let rowIndex: number;
+let cellIndex: number;
+
+// [
+//  [td, td, td],
+//  [td, td, td],
+//  [td, td, td],
+// ]
+
+function checkWinner(target: EventTarget) {
+  rows.forEach((row, ri) => {
+    row.forEach((cell, ci) => {
+      if (cell === target) {
+        rowIndex = ri;
+        cellIndex = ci;
+      }
+    });
+  });
+  // 세칸 모두 채워졌는가?
+  let hasWinner: boolean = false;
+
+  console.log(rows[rowIndex][0]);
+  console.log(rows[rowIndex][1]);
+  console.log(rows[rowIndex][2]);
+
+  // 가로줄 검사
+  if (
+    rows[rowIndex][0].textContent === turn &&
+    rows[rowIndex][1].textContent === turn &&
+    rows[rowIndex][2].textContent === turn
+  ) {
+    hasWinner = true;
+  }
+  // 세로줄 검사
+  if (
+    rows[0][cellIndex].textContent === turn &&
+    rows[1][cellIndex].textContent === turn &&
+    rows[2][cellIndex].textContent === turn
+  ) {
+    hasWinner = true;
+  }
+  // 대각선 검사
+  if (
+    rows[0][0].textContent === turn &&
+    rows[1][1].textContent === turn &&
+    rows[2][2].textContent === turn
+  ) {
+    hasWinner = true;
+  }
+  if (
+    rows[0][2].textContent === turn &&
+    rows[1][1].textContent === turn &&
+    rows[2][0].textContent === turn
+  ) {
+    hasWinner = true;
+  }
+  return hasWinner;
+}
+
 function checkFilledBlock(event: Event) {
   // 칸에 글자가 있나?
   if ((event.target as HTMLTableCellElement).textContent) {
@@ -15,6 +74,26 @@ function checkFilledBlock(event: Event) {
     return;
   }
   (event.target as HTMLTableCellElement).textContent = turn;
+
+  // 승부 판별하기
+  if (checkWinner(event.target as HTMLTableCellElement)) {
+    $result.textContent = `${turn}님이 승리!`;
+    $table.removeEventListener('click', checkFilledBlock);
+    return;
+  }
+  // 무승부 검사
+  let draw = true;
+  rows.forEach((row) => {
+    row.forEach((cell) => {
+      if (!cell.textContent) {
+        draw = false;
+      }
+    });
+  });
+  if (draw) {
+    $result.textContent = '무승부';
+    return;
+  }
   // 차례 넘기기
   turn = turn === '😼' ? '🐶' : '😼';
 }
