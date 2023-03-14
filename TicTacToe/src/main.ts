5e00ae0db1b47794519482adac210297942cd11e
Jinnie <hjk7639@gmail.com>
2023-03-14 09:52:37 +0900

    이미 선택된 칸인지 체크 & 차례 넘기기 로직 구현

--- TicTacToe/src/main.ts
+++ TicTacToe/src/main.ts
@@ -2,11 +2,27 @@ import './style.css';
 
 const $table = document.createElement('table') as HTMLTableElement;
 
+let turn = '😼';
+
 for (let i = 0; i < 3; i++) {
   const $tr = document.createElement('tr') as HTMLTableRowElement;
   for (let k = 0; k < 3; k++) {
     const $td = document.createElement('td') as HTMLTableCellElement;
+    $td.addEventListener('click', (event: Event) => {
+      // 칸에 글자가 있나?
+      if ((event.currentTarget as HTMLTableCellElement).textContent) {
+        return;
+      } else {
+        (event.currentTarget as HTMLTableCellElement).textContent = turn;
+      }
 
+      // 차례 넘기기
+      if (turn === '😼') {
+        turn = '🐶';
+      } else if (turn === '🐶') {
+        turn = '😼';
+      }
+    });
     $tr.append($td);
   }
   $table.append($tr);
