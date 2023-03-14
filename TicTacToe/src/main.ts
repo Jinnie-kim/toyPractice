df95205607be1abb4b21d51ef1d26370bb1b2ddb
Jinnie <hjk7639@gmail.com>
2023-03-14 09:36:59 +0900

    틱택토 게임 3x3 화면 그리기

--- TicTacToe/src/main.ts
+++ TicTacToe/src/main.ts
@@ -1,1 +1,15 @@
 import './style.css';
+
+const $table = document.createElement('table') as HTMLTableElement;
+
+for (let i = 0; i < 3; i++) {
+  const $tr = document.createElement('tr') as HTMLTableRowElement;
+  for (let k = 0; k < 3; k++) {
+    const $td = document.createElement('td') as HTMLTableCellElement;
+
+    $tr.append($td);
+  }
+  $table.append($tr);
+}
+
+document.body.append($table);
