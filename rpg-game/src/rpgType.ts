2a5bacf6830ca0886b210ae24199195d49c03bba
Jinnie <hjk7639@gmail.com>
2023-03-15 09:58:48 +0900

    주인공, 몬스터 stat 객체 type을 파일로 분리하여 작성

---
+++ rpg-game/src/rpgType.ts
@@ -0,0 +1,15 @@
+export interface HeroStat {
+  name: string;
+  lev: number;
+  maxHp: number;
+  hp: number;
+  xp: number;
+  att: number;
+}
+
+export interface MonsterStat {
+  name: string;
+  hp: number;
+  att: number;
+  xp: number;
+}
