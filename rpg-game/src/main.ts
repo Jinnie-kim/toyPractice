b7d9ed6c9920c160fae85e809aa90371efd2e0bf
Jinnie <hjk7639@gmail.com>
2023-03-15 15:06:45 +0900

    유저 닉네임을 받고 게임을 싲가하고 유저 stat을 업데이트 하는 로직을 전부 클래스 형식으로 전환

--- rpg-game/src/main.ts
+++ rpg-game/src/main.ts
@@ -17,96 +17,157 @@ const $monsterAtt = document.querySelector('#monster-att') as HTMLSpanElement;
 // 게임 상황 메시지
 const $message = document.querySelector('#message') as HTMLDivElement;
 
-const hero: HeroStat = {
-  name: '',
-  lev: 1,
-  maxHp: 100,
-  hp: 100,
-  xp: 0,
-  att: 10,
-  attack(monster) {
-    monster.hp -= this.att;
-    this.hp -= monster.att;
-  },
+// Game을 총괄하는 클래스
+class Game {
+  monster: MonsterStat | null;
+  hero: HeroStat | null;
+  monsterList: MonsterStat[];
+
+  constructor(name: string) {
+    this.monster = null;
+    this.hero = null;
+    this.monsterList = [
+      {
+        name: '슬라임',
+        hp: 25,
+        att: 10,
+        xp: 10,
+      },
+      {
+        name: '스켈레톤',
+        hp: 50,
+        att: 15,
+        xp: 20,
+      },
+      {
+        name: '마왕',
+        hp: 150,
+        att: 35,
+        xp: 50,
+      },
+    ];
+    this.start(name);
+  }
+
+  start(name: string) {
+    $gameMenu.addEventListener('submit', this.onGameMenuInput);
+    $battleMenu.addEventListener('submit', this.onBattleMenuInput);
+    this.changeScreen('game');
+    this.hero = new Hero(this, name);
+    this.updateHeroStat();
+  }
+  changeScreen(screen: string) {
+    if (screen === 'start') {
+      $startScreen.style.display = 'block';
+      $gameMenu.style.display = 'none';
+      $battleMenu.style.display = 'none';
+    } else if (screen === 'game') {
+      $startScreen.style.display = 'none';
+      $gameMenu.style.display = 'block';
+      $battleMenu.style.display = 'none';
+    } else if (screen === 'battle') {
+      $startScreen.style.display = 'none';
+      $gameMenu.style.display = 'none';
+      $battleMenu.style.display = 'block';
+    }
+  }
+  onGameMenuInput = (event: Event) => {
+    event.preventDefault();
+    const input = (event.target as HTMLFormElement)['menu-input'].value;
+    if (input === '1') {
+      // 모험
+      this.changeScreen('battle');
+    } else if (input === '2') {
+      // 휴식
+    } else if (input === '3') {
+      // 종료
+    }
+  };
+
+  onBattleMenuInput = (event: Event) => {
+    event.preventDefault();
+    const input = (event.target as HTMLFormElement)['battle-input'].value;
+    if (input === '1') {
+      // 공격
+    } else if (input === '2') {
+      // 회복
+    } else if (input === '3') {
+      // 도망
+      this.changeScreen('game');
+    }
+  };
+
+  updateHeroStat() {
+    const { hero } = this;
+    if (hero === null) {
+      $heroName.textContent = '';
+      $heroLevel.textContent = '';
+      $heroHp.textContent = '';
+      $heroXp.textContent = '';
+      $heroAtt.textContent = '';
+      return;
+    }
+    $heroName.textContent = hero.name;
+    $heroLevel.textContent = `${hero.lev}Lev`;
+    $heroHp.textContent = `HP: ${hero.hp}/${hero.maxHp}`;
+    $heroXp.textContent = `XP: ${hero.xp}/${15 * hero.lev}`;
+    $heroAtt.textContent = `ATT: ${hero.att}`;
+  }
+}
+
+class Hero {
+  game: Game;
+  name: string;
+  lev: number;
+  maxHp: number;
+  hp: number;
+  xp: number;
+  att: number;
+
+  constructor(game: Game, name: string) {
+    this.game = game; // game과 hero를 엮어주기 위해서
+    this.name = name;
+    this.lev = 1;
+    this.maxHp = 100;
+    this.hp = 100;
+    this.xp = 0;
+    this.att = 10;
+    // this.game.updateHeroStat(); Hero안에서 게임 클래스에 접근해서 업데이트를 해도 된다.
+  }
+
+  attack(target) {
+    target.hp -= this.att;
+  }
   heal(monster) {
     this.hp += 20;
     this.hp -= monster.att;
-  },
-};
+  }
+}
 
-let monster: MonsterStat | null = null;
+class Monster {
+  game: Game;
+  name: string;
+  maxHp: number;
+  hp: number;
+  att: number;
+  xp: number;
+  constructor(game: Game, name: string, hp: number, att: number, xp: number) {
+    this.game = game;
+    this.name = name;
+    this.maxHp = hp;
+    this.hp = hp;
+    this.xp = xp;
+    this.att = att;
+  }
+  attack(target) {
+    target.hp -= this.att;
+  }
+}
 
-const monsterList: MonsterStat[] = [
-  {
-    name: '슬라임',
-    hp: 25,
-    att: 10,
-    xp: 10,
-  },
-  {
-    name: '스켈레톤',
-    hp: 50,
-    att: 15,
-    xp: 20,
-  },
-  {
-    name: '마왕',
-    hp: 150,
-    att: 35,
-    xp: 50,
-  },
-];
+let game = null;
 
 $startScreen.addEventListener('submit', (event: Event) => {
   event.preventDefault();
-  const name: string = (event.target as HTMLFormElement)['name-input'].value;
-  $startScreen.style.display = 'none';
-  $gameMenu.style.display = 'block';
-  $heroName.textContent = name;
-  $heroLevel.textContent = `${hero.lev}Lev`;
-  $heroHp.textContent = `HP: ${hero.hp}/${hero.maxHp}`;
-  $heroXp.textContent = `XP: ${hero.xp}/${15 * hero.lev}`;
-  $heroAtt.textContent = `ATT: ${hero.att}`;
-  hero.name = name;
-});
-
-$gameMenu.addEventListener('submit', (event: Event) => {
-  event.preventDefault();
-  const input = (event.target as HTMLFormElement)['menu-input'].value;
-  if (input === '1') {
-    // 모험
-    $gameMenu.style.display = 'none';
-    $battleMenu.style.display = 'block';
-    monster = JSON.parse(
-      JSON.stringify(
-        monsterList[Math.floor(Math.random() * monsterList.length)]
-      )
-    );
-    if (monster === null) return;
-    monster.maxHp = monster.hp;
-    $monsterName.textContent = monster.name;
-    $monsterHp.textContent = `HP: ${monster.hp}/${monster.maxHp}`;
-    $monsterAtt.textContent = `ATT: ${monster.att}`;
-  } else if (input === 2) {
-    // 휴식
-  } else if (input === 3) {
-    // 종료
-  }
-});
-
-$battleMenu.addEventListener('submit', (event: Event) => {
-  const input = (event.target as HTMLFormElement)['battle-menu'].value;
-  if (input === '1') {
-    // 공격
-    if (monster === null) return;
-    hero.attack(monster);
-    monster.attack(hero);
-    $heroHp.textContent = `HP: ${hero.hp}/${hero.maxHp}`;
-    $monsterHp.textContent = `HP: ${monster.hp}/${monster.maxHp}`;
-    $message.textContent = `${hero.att}의 데미지를 주고, ${monster.att}의 데미지를 받았다.`;
-  } else if (input === '2') {
-    // 회복
-  } else if (input === '3') {
-    // 도망
-  }
+  const name = (event.target as HTMLFormElement)['name-input'].value;
+  game = new Game(name);
 });
