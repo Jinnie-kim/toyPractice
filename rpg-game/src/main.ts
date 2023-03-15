91a35748805b0af47956f97db4b5634255a72473
Jinnie <hjk7639@gmail.com>
2023-03-15 10:20:54 +0900

    배틀 메뉴 submit 이벤트 리스너 구조 짜기

--- rpg-game/src/main.ts
+++ rpg-game/src/main.ts
@@ -82,3 +82,11 @@ $gameMenu.addEventListener('submit', (event: Event) => {
   } else if (input === 3) {
   }
 });
+
+$battleMenu.addEventListener('submit', (event: Event) => {
+  const input = (event.target as HTMLFormElement)['battle-menu'].value;
+  if (input === '1') {
+  } else if (input === '2') {
+  } else if (input === '3') {
+  }
+});
