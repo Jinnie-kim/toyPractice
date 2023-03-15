d302a4f18d0b3d0629193d13a8f5f80ab72fd379
Jinnie <hjk7639@gmail.com>
2023-03-15 09:32:44 +0900

    rpg게임 닉네임을 입력받아 주인공을 만들고 게임 모드 선택 전환으로 넘어가는 로직 구현

--- rpg-game/src/main.ts
+++ rpg-game/src/main.ts
@@ -1,1 +1,14 @@
 import './style.css';
+
+const $startScreen = document.querySelector('#start-screen') as HTMLFormElement;
+const $gameMenu = document.querySelector('#game-menu') as HTMLFormElement;
+const $battleMenu = document.querySelector('#battle-menu') as HTMLFormElement;
+const $heroName = document.querySelector('#hero-name') as HTMLSpanElement;
+
+$startScreen.addEventListener('submit', (event: Event) => {
+  event.preventDefault();
+  const name: string = (event.target as HTMLFormElement)['name-input'].value;
+  $startScreen.style.display = 'none';
+  $gameMenu.style.display = 'block';
+  $heroName.textContent = name;
+});
