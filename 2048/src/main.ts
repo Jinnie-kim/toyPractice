b5c10a629cea1996c12127ef7f64f8ba43a3a1a3
Jinnie <hjk7639@gmail.com>
2023-03-21 15:24:12 +0900

    게임 규칙대로 숫자 합쳐 두 배로 만들고 새로운 2 추가 하기

--- 2048/src/main.ts
+++ 2048/src/main.ts
@@ -56,35 +56,142 @@ function draw() {
 
 startGame();
 
-function moveCells(direction: string) {
-  window.addEventListener('keyup', (event) => {
-    if (event.key === 'Arrowup') {
-      moveCells('up');
-    } else if (event.key === 'ArrowDown') {
-      moveCells('down');
-    } else if (event.key === 'ArrowLeft') {
-      moveCells('left');
-    } else if (event.key === 'ArrowRight') {
-      moveCells('right');
-    }
-  });
+// dummy data
+// data = [
+//   [0, 2, 4, 2],
+//   [0, 0, 8, 0],
+//   [2, 2, 2, 2],
+//   [0, 16, 0, 4],
+// ];
+// draw();
 
-  let startCoord: number[];
-  window.addEventListener('mousedown', (event) => {
-    startCoord = [event.clientX, event.clientY];
-  });
-  window.addEventListener('mouseup', (event) => {
-    const endCoord = [event.clientX, event.clientY];
-    const diffX = endCoord[0] - startCoord[0];
-    const diffY = endCoord[1] - startCoord[1];
-    if (diffX < 0 && Math.abs(diffX) > Math.abs(diffY)) {
-      moveCells('left');
-    } else if (diffX > 0 && Math.abs(diffX) > Math.abs(diffY)) {
-      moveCells('right');
-    } else if (diffY > 0 && Math.abs(diffX) <= Math.abs(diffY)) {
-      moveCells('down');
-    } else if (diffY < 0 && Math.abs(diffX) <= Math.abs(diffY)) {
-      moveCells('up');
+function moveCells(direction: string) {
+  switch (direction) {
+    case 'left': {
+      const newData: number[][] = [[], [], [], []];
+      data.forEach((rowData, i) => {
+        rowData.forEach((cellData, j) => {
+          if (cellData) {
+            const currentRow = newData[i];
+            const prevData = currentRow[currentRow.length - 1];
+            if (prevData === cellData) {
+              // 이전 값과 지금 값이 같으면
+              currentRow[currentRow.length - 1] *= -2; // 한꺼번에 모두 합쳐지지 않도록 음수기호롤 붙여서 다른 값으로 인식하도록 만듦
+            } else {
+              newData[i].push(cellData);
+            }
+          }
+        });
+      });
+      console.log(newData);
+      [1, 2, 3, 4].forEach((rowData, i) => {
+        [1, 2, 3, 4].forEach((cellData, j) => {
+          data[i][j] = Math.abs(newData[i][j]) || 0; // 원래 데이터로 넣을 때는 다시 절댓값으로 양수로 만듦
+        });
+      });
+      break;
     }
-  });
+    case 'right': {
+      const newData: number[][] = [[], [], [], []];
+      data.forEach((rowData, i) => {
+        rowData.forEach((cellData, j) => {
+          if (rowData[3 - j]) {
+            const currentRow = newData[i];
+            const prevData = currentRow[currentRow.length - 1];
+            if (prevData === rowData[3 - j]) {
+              currentRow[currentRow.length - 1] *= -2;
+            } else {
+              newData[i].push(rowData[3 - j]);
+            }
+          }
+        });
+      });
+      console.log(newData);
+      [1, 2, 3, 4].forEach((rowData, i) => {
+        [1, 2, 3, 4].forEach((cellData, j) => {
+          data[i][3 - j] = Math.abs(newData[i][j] || 0);
+        });
+      });
+      break;
+    }
+    case 'up': {
+      const newData: number[][] = [[], [], [], []];
+      data.forEach((rowData, i) => {
+        rowData.forEach((cellData, j) => {
+          if (cellData) {
+            const currentRow = newData[j];
+            const prevData = currentRow[currentRow.length - 1];
+            if (prevData === cellData) {
+              currentRow[currentRow.length - 1] *= -2;
+            } else {
+              newData[j].push(cellData);
+            }
+          }
+        });
+      });
+      console.log(newData);
+      [1, 2, 3, 4].forEach((cellData, i) => {
+        [1, 2, 3, 4].forEach((rowData, j) => {
+          data[j][i] = Math.abs(newData[i][j]) || 0;
+        });
+      });
+      break;
+    }
+    case 'down': {
+      const newData: number[][] = [[], [], [], []];
+      data.forEach((rowData, i) => {
+        rowData.forEach((cellData, j) => {
+          if (data[3 - i][j]) {
+            const currentRow = newData[j];
+            const prevData = currentRow[currentRow.length - 1];
+            if (prevData === data[3 - i][j]) {
+              currentRow[currentRow.length - 1] *= -2;
+            } else {
+              newData[j].push(data[3 - i][j]);
+            }
+          }
+        });
+      });
+      console.log(newData);
+      [1, 2, 3, 4].forEach((cellData, i) => {
+        [1, 2, 3, 4].forEach((rowData, j) => {
+          data[3 - j][i] = Math.abs(newData[i][j]) || 0;
+        });
+      });
+      break;
+    }
+  }
+  put2ToRandomCell();
+  draw();
 }
+
+window.addEventListener('keyup', (event) => {
+  if (event.key === 'Arrowup') {
+    moveCells('up');
+  } else if (event.key === 'ArrowDown') {
+    moveCells('down');
+  } else if (event.key === 'ArrowLeft') {
+    moveCells('left');
+  } else if (event.key === 'ArrowRight') {
+    moveCells('right');
+  }
+});
+
+let startCoord: number[];
+window.addEventListener('mousedown', (event) => {
+  startCoord = [event.clientX, event.clientY];
+});
+window.addEventListener('mouseup', (event) => {
+  const endCoord = [event.clientX, event.clientY];
+  const diffX = endCoord[0] - startCoord[0];
+  const diffY = endCoord[1] - startCoord[1];
+  if (diffX < 0 && Math.abs(diffX) > Math.abs(diffY)) {
+    moveCells('left');
+  } else if (diffX > 0 && Math.abs(diffX) > Math.abs(diffY)) {
+    moveCells('right');
+  } else if (diffY > 0 && Math.abs(diffX) <= Math.abs(diffY)) {
+    moveCells('down');
+  } else if (diffY < 0 && Math.abs(diffX) <= Math.abs(diffY)) {
+    moveCells('up');
+  }
+});
