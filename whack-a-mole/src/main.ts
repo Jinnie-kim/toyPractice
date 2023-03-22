ead960ff01d46ce3db28f735265d54b73774d014
Jinnie <hjk7639@gmail.com>
2023-03-22 15:25:14 +0900

    두더지 잡기 게임 구조 셋팅

--- whack-a-mole/src/main.ts
+++ whack-a-mole/src/main.ts
@@ -1,1 +1,7 @@
 import './style.css';
+
+const $timer = document.querySelector('#timer') as HTMLSpanElement;
+const $score = document.querySelector('#score') as HTMLSpanElement;
+const $game = document.querySelector('#game') as HTMLDivElement;
+const $start = document.querySelector('#start') as HTMLButtonElement;
+const $$cells = document.querySelectorAll('.cell');
