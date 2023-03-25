b601437ce232166180307505ef7f219c177baeec
Jinnie <hjk7639@gmail.com>
2023-03-25 13:44:51 +0900

    useCallback을 이용하여 함수 재생성 방지

--- udemy-react/behindReact/src/App.js
+++ udemy-react/behindReact/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Button from './components/UI/Button/Button';
 
 import './App.css';
@@ -6,17 +6,26 @@ import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('App Running');
 
-  const onClick = () => {
-    setShowParagraph((prevState) => !prevState);
+  const onClick = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevState) => !prevState);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   };
+
   return (
     <>
       <div className="app">
         <h1>Hi there!</h1>
-        <DemoOutput show={false} />
+        <DemoOutput show={showParagraph} />
+        <Button onClick={allowToggleHandler}>Allow Toggling</Button>
         <Button onClick={onClick}>show Paragraph</Button>
       </div>
     </>
